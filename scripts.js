3be979ab79678cbb2f1a67f562f8c0f04b81d9be
Jose Romero <jose.arcangel.romero@gmail.com>
2019-04-01 19:38:55 -0400

    add backbone models

--- scripts.js
+++ scripts.js
@@ -7,3 +7,34 @@ var Blog = Backbone.Model.extend({
     url: ""
   }
 });
+
+var Blogs = Backbone.Collection.extend({});
+
+//create two new blogs
+var blog1 = new Blog({
+  author: "Micheal",
+  title: "Micheal's Blog",
+  url: "http://michealsblog.com"
+});
+
+var blog2 = new Blog({
+  author: "John",
+  title: "John's Blog",
+  url: "http://johnsblog.com"
+});
+
+//collection
+var blogs = new Blogs([blog1, blog2]);
+
+//backbone views for a single blob
+var BlogView = Backbone.View.extend({
+  model:new Blog(),
+  tagName:"tr",
+  intialize ;function(){
+    this.template= _.template($('.blogs-list-template'))
+  }
+});
+
+
+//all logs
+var BlogsView = Backbone.View.extend({});
